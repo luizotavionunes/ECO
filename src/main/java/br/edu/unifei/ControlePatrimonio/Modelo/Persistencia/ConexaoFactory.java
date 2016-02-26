9893e29a823d9224decd5630f9438f5b160e2ba0
Branieri <bruno_ranieri@hotmail.com>
2016-02-26 12:16:33 -0300

    Retirando testes SYSO

--- src/main/java/br/edu/unifei/ControlePatrimonio/Modelo/Persistencia/ConexaoFactory.java
+++ src/main/java/br/edu/unifei/ControlePatrimonio/Modelo/Persistencia/ConexaoFactory.java
@@ -9,14 +9,12 @@ public class ConexaoFactory {
 
 	public static Connection getConnection() {
 
-		System.out.println("Conectando ao banco");
 
 		try {
 
 			
 				//Class.forName("com.mysql.jdbc.Driver");
 				 DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-				System.out.println("Conectado ao banco");
 				return DriverManager.getConnection("jdbc:mysql://localhost:3306/controle_lab_eco_bd", "root", "root");
 
 		
@@ -24,7 +22,7 @@ public class ConexaoFactory {
 		
 			
 		} catch (SQLException e) {
-			System.out.println("Erro ao conectar ao banco 2.");
+			System.out.println("Erro ao conectar ao banco.");
 			throw new RuntimeException(e);
 
 		}
